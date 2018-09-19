d5ff101dac2c6826882a240d7a41e729e5abe1cc
T_dak <thilandakshina@gmail.com>
2018-09-19 23:50:26 +0530

    New changes with view

---
+++ StudentController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using LYC.StdMgt.BusinessService.Contracts;
+using LYC.StdMgt.Domain;
+using LYC.StdMgt.Utils.Log.Contracts;
+using LYC.StdMgt.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LYC.StdMgt.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        private IStudentManager _studentManager;
+        private ILogger _logger;
+        public StudentController(IStudentManager studentManager, ILogger logger)
+        {
+            _studentManager = studentManager;
+            _logger = logger;
+        }
+
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> student = new List<Student>();
+
+            student = _studentManager.GetStudentList();
+
+            List<StudentViewModel> studentViewModel = Mapper.Map<List<Student>, List<StudentViewModel>>(student);
+
+
+            return View(studentViewModel);
+        }
+
+        public ActionResult Create(StudentViewModel studentViewModel)
+        {
+            if (studentViewModel.FirstName != null)
+            {
+                Student student = Mapper.Map<StudentViewModel, Student>(studentViewModel);
+                _studentManager.AddStudent(student);
+                return RedirectToAction("Index", "Student");
+            }
+          
+            return View();
+
+        }
+       
+    }
+}
